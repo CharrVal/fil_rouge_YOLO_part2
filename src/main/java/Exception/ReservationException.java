85d0a8eec8ec72b2100f781952616636e2350d53
Valentin <valentinhots2@gmail.com>
2025-02-20 13:41:00 +0100

    Reservationbll update

--- src/main/java/Exception/ReservationException.java
+++ src/main/java/Exception/ReservationException.java
@@ -1,9 +1,22 @@
 package Exception;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReservationException extends Exception {
 	private static final long serialVersionUID = 1L;
 
-	public ReservationException(String message) {
-		super(message);
+	private List<String> messages;
+	
+	public ReservationException() {
+		messages = new ArrayList<>();
+	}
+	
+	public void ajouterErreur(String erreur) {
+		messages.add(erreur);
+	}
+	
+	public List<String> getMessages() {
+		return messages;
 	}
 }
